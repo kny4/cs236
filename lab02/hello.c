cd1768b7858fa13eea11bd5f3d189659559e2a56
kny4 <71412566+kny4@users.noreply.github.com>
2022-01-19 12:13:31 -0500

    Add files via upload

---
+++ lab02/hello.c
@@ -0,0 +1,12 @@
+/* My first cross compiled Raspberry Pi program
+ Name: your name
+*/
+#include <stdio.h>
+int main(void)
+{
+ printf("Hello world\n");
+ printf("Compiled by an Intel processor and\n");
+ printf("running on an ARM processor.\n");
+ return 0;
+}
+
